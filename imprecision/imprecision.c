d8328e27b1b678dd20dae722fc4a5f14bdf2d8ef
Edward Dupre <edwd42@gmail.com>
2017-10-25 19:19:26 +0000

    initial commit

---
+++ imprecision/imprecision.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+    double imprecision = 5.1 / 2.0;
+    printf("%.65f\n", imprecision);
+}
---
+++ pset1/helloWorld/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// First assignment pset1
+int main(void)
+{
+    printf("hello, world\n");
+}
