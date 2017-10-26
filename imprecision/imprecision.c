b22e98b2e35ab0c40b2e2dbcdcd8ae60f8117e0c
Edward Dupre <edwd42@gmail.com>
2017-10-26 02:01:21 +0000

    completed pset1/mario less

--- imprecision/imprecision.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    double imprecision = 5.1 / 2.0;
-    printf("%.65f\n", imprecision);
-}
---
+++ pset1/mario/mario.c
@@ -0,0 +1,47 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void printSpace(int nTimes);
+void printHash(int nTimes);
+
+// Assignment pset1/water
+int main(void)
+{
+    // Get input from user
+    int height;
+    do
+    {
+        printf("Height: ");
+        height = get_int();
+
+    }
+    while (height < 0 || height > 23);
+
+    // Send spaces and hashes to print function
+    for (int i = 2; i <= height+1; i++)
+    {
+        printSpace((height-i)+1);
+        printHash(i);
+        printf("\n");
+    }
+
+}
+
+
+void printSpace(int nTimes)
+{
+    // Print number of spaces
+    for (int i = 0; i < nTimes; i++)
+    {
+        printf(" ");
+    }
+}
+
+void printHash(int nTimes)
+{
+    // Print number of hashes
+    for (int i = 0; i < nTimes; i++)
+    {
+        printf("#");
+    }
+}
