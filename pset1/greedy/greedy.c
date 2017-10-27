58424c458e05875bdd1ccaa36d1132850b660b28
Edward Dupre <edwd42@gmail.com>
2017-10-27 13:59:33 +0000

    commit before making new dev branch

--- pset1/greedy/greedy.c
+++ pset1/greedy/greedy.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 
+int getCoins(float change);
+
 // Assignment pset1/greedy
 int main(void)
 {
@@ -15,48 +17,80 @@ int main(void)
     }
     while (change < 0);
 
-    if (change > 0.25)
+    printf("Change == %f\n",change);
+    int coins = getCoins(change);
+    printf("Coins == %d\n",coins);
+}
+
+
+int getCoins(float change){
+    // Returns the number of coins
+    int nQuarters = 0;
+    int nDimes = 0;
+    int nNickles = 0;
+    int nPennies = 0;
+    int coins = nQuarters + nDimes + nNickles + nPennies;
+    if (change >= 0.25)
     {
         // do modulo
-        if (fmod(change, 0.25) == 0)
+        float rmd = (fmodf(change, 0.25));
+        printf("32.outside if(rmd == 0) %f\n", rmd);
+        if (rmd == 0)
         {
             // change is a multiple of .25
-            int coins = change / .25;
-            printf("%d\n", coins);
-            return 0;
+            coins += change / .25;
+            // printf("%d\n", coins);
+            printf("37.inside if(rmd == 0) %f\n", rmd);
+            return coins;
+        }
+        else
+        {
+            float baseCoin = change - rmd;
+            nQuarters = baseCoin / .25;
+            printf("remainingChange == %f\n", rmd);
+            getCoins(rmd);
         }
     }
 
-    // if (change > 0.10)
-    // {
-    //     // do modulo
-    //     if ((int)change % .10 == 0)
-    //     {
-    //         // change is a multiple of .10
-    //         // change < .25
-    //         return (int)change / (int).10;
-    //     }
-    // }
+    else if (change >= 0.10)
+    {
+        // do modulo
+        float rmd = (fmodf(change, 0.10));
+        if (rmd == 0)
+        {
+            // change is a multiple of .10
+            coins = change / .10;
+            // printf("coins == %d\n", coins);
+            return coins;
+        }
+    }
 
-    // if (change > 0.05)
-    // {
-    //     // do modulo
-    //     if ((int)change % .05 == 0)
-    //     {
-    //         // change is a multiple of .05
-    //         // change < .10
-    //         return (int)change / (int).05;
-    //     }
-    // }
+    else if (change >= 0.05)
+    {
+        // do modulo
+        float rmd = (fmodf(change, 0.05));
+        if (rmd == 0)
+        {
+            // change is a multiple of .05
+            // change < .10
+            coins = change / .05;
+            // printf("coins == %d\n", coins);
+            return coins;
+        }
+    }
 
-    // if (change > 0.01)
-    // {
-    //     // do modulo
-    //     if ((int)change % .01 == 0)
-    //     {
-    //         // change is a multiple of .01
-    //         // change < .05
-    //         return (int)change / (int).01;
-    //     }
-    // }
+    else  // change < .05
+    {
+        // do modulo
+        float rmd = (fmodf(change, 0.01));
+        if (rmd == 0)
+        {
+            // change is a multiple of .01
+            coins = change / .01;
+            // printf("coins == %d\n", coins);
+            return coins;
+        }
+    }
+
+    return 1;
 }
