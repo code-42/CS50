11907c134e215b61630b51baa14f3f02fa619e0a
Edward Dupre <edwd42@gmail.com>
2017-10-30 02:02:07 +0000

    add more comments to pset2/cipher

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -26,28 +26,37 @@ int main(int argc, string argv[])
     string p = get_string();
 
     printf("ciphertext: ");
+
+    // Loop through the plaintext string
     for (int i = 0, n = strlen(p); i < n; i++)
     {
+        // Check that the character is a letter
         if ((p[i] >= 'A' && p[i] <= 'Z') || (p[i] >= 'a' && p[i] <= 'z'))
         {
+            // Determine if the character is uppercase
             if (p[i] >= 'A' && p[i] <= 'Z')
             {
-                //
+                // Calculate ciher for uppercase letter
                 printf("%c", ((p[i] + k - 'A') % 26) + 'A');
             }
 
+            // Determine if the character is lowercase
             if (p[i] >= 'a' && p[i] <= 'z')
             {
-                //
+                // Calculate ciher for lowercase letter
                 printf("%c", ((p[i] + k - 'a') % 26) + 'a');
             }
         }
         else
         {
+            // Print characters that are not letters
             printf("%c", p[i]);
         }
     }
 
+    // After outputting ciphertext, you should print a newline
     printf("\n");
+
+    // Your program should then exit by returning 0 from main
     return 0;
 }
