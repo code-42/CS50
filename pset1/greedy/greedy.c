22229279bdc29770a9653329f159140a4155c612
Edward Dupre <edwd42@gmail.com>
2017-11-05 22:45:12 +0000

    fixed style and added comments

--- pset1/greedy/greedy.c
+++ pset1/greedy/greedy.c
@@ -2,50 +2,62 @@
 #include <stdio.h>
 #include <math.h>
 
+// Initialize prototype function
 int getCoins(float change);
 
 // Assignment pset1/greedy
 int main(void)
 {
-    // Get input from user
+    // Initialize variable
     float change;
+
+    // Get input from user
     do
     {
         printf("O hai! How much change is owed? ");
         change = get_float();
     }
-
     while (change < 0);
 
+    // Initialize variable and call function
     int coins = getCoins(change);
-    printf("%d\n",coins);
+
+    // Print results
+    printf("%d\n", coins);
 }
 
 
 // Returns the number of coins
-int getCoins(float change){
+int getCoins(float change)
+{
 
     // First convert float to int
-    change = round(change*100);
+    change = round(change * 100);
     // Now we are working with an int
 
     int coins = 0;
-    while(change >= 25)
+
+    // Loop through decreasing coin sizes
+    while
+    (change >= 25)
     {
         change = change - 25;
         coins++;
     }
-    while(change >= 10)
+    while
+    (change >= 10)
     {
         change = change - 10;
         coins++;
     }
-    while(change >= 5)
+    while
+    (change >= 5)
     {
         change = change - 5;
         coins++;
     }
-    while(change >= 1)
+    while
+    (change >= 1)
     {
         change = change - 1;
         coins++;
