f6032fa2fc78db08c08c5b17e897afcd7d388be2
Edward Dupre <edwd42@gmail.com>
2017-11-07 19:40:03 +0000

    implement bubble sort in helpers.c

--- pset3/find/less/helpers.c
+++ pset3/find/less/helpers.c
@@ -15,6 +15,7 @@ bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
     // Arguments are needle, haystack, size
+    // When passing an array to a function, you have to pass in its size separately
 
     if ( n < 1 )
     {
@@ -24,7 +25,7 @@ bool search(int value, int values[], int n)
     // Linear search
     for (int i = 0; i < n ; i++)
     {
-                if (values[i] == value)
+        if (values[i] == value)
         {
             return true;
         }
@@ -37,10 +38,28 @@ bool search(int value, int values[], int n)
 /**
  * Sorts array of n values.
  */
- // values is haystack[], int is size of haystack
+// values is haystack[], int is size of haystack
+// When passing an array to a function, you have to pass in its size separately
 void sort(int values[], int n)
 {
     // TODO: implement a sorting algorithm
-
+    // Bubble sort
+    int i, j;
+    // Loop through values array
+    for (i = 0; i < n - 1; i++)
+    {
+        // Loop through remaining elements
+        for (j = 0; j < n - i - 1; j++)
+        {
+            // If current element is greater than the next element
+            if (values[j] > values[j + 1])
+            {
+                // Swap elements
+                int temp = values[j];
+                values[j] = values[j + 1];
+                values[j + 1] = temp;
+            }
+        }
+    }
     return;
 }
