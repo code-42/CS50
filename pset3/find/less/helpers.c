65808651a60c615c3856960bb03630a54ce30e44
Edward Dupre <edwd42@gmail.com>
2017-11-15 02:22:09 +0000

    implemented bubble sort a totally different way and it passes check50 also

--- pset3/find/less/helpers.c
+++ pset3/find/less/helpers.c
@@ -42,26 +42,76 @@ bool search(int value, int values[], int n)
  */
 // values is haystack[], int is size of haystack
 // When passing an array to a function, you have to pass in its size separately
+// void sort(int values[], int n)
+// {
+//     // TODO: implement a sorting algorithm
+//     // Bubble sort
+//     int i, j;
+//     // Loop through values array
+//     for (i = 0; i < n - 1; i++)
+//     {
+//         // Loop through remaining elements
+//         for (j = 0; j < n - i - 1; j++)
+//         {
+//             // If current element is greater than the next element
+//             if (values[j] > values[j + 1])
+//             {
+//                 // Swap elements
+//                 int temp = values[j];
+//                 values[j] = values[j + 1];
+//                 values[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return;
+// }
+
+
 void sort(int values[], int n)
 {
-    // TODO: implement a sorting algorithm
-    // Bubble sort
-    int i, j;
-    // Loop through values array
-    for (i = 0; i < n - 1; i++)
-    {
-        // Loop through remaining elements
-        for (j = 0; j < n - i - 1; j++)
-        {
-            // If current element is greater than the next element
-            if (values[j] > values[j + 1])
-            {
-                // Swap elements
-                int temp = values[j];
-                values[j] = values[j + 1];
-                values[j + 1] = temp;
-            }
-        }
-    }
-    return;
+  int swapCounter;
+  int i;
+
+  do
+  {
+      swapCounter = 0;
+      for(i = 0; i < n - 1; i++)
+      {
+          if (values[i] > values[i + 1])
+          {
+            int temp = values[i];
+            values[i] = values[i + 1];
+            values[i + 1] = temp;
+            swapCounter = 1;
+          }
+
+      }
+  }
+  while(swapCounter != 0);
+
+  return;
 }
+
+// void sort(int values[], int n)
+// {
+//     // TODO: implement a sorting algorithm
+//     // Bubble sort
+//     int i, j;
+//     // Loop through values array
+//     for (i = 0; i < n; i++)
+//     {
+//         // Loop through remaining elements
+//         for (j = 0; j < n - 1; j++)
+//         {
+//             // If current element is greater than the next element
+//             if (values[j] > values[j + 1])
+//             {
+//                 // Swap elements
+//                 int temp = values[j +  1];
+//                 values[j + 1] = values[j];
+//                 values[j] = temp;
+//             }
+//         }
+//     }
+//     return;
+// }
