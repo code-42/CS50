a4d58657630df7188226b301d74e53ccd08a0595
Edward Dupre <edwd42@gmail.com>
2018-04-11 02:18:13 +0000

    implement load and read dictionary file

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,21 +1,33 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cs50.h>
 
 #include "dictionary.h"
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    fprintf(stderr, "unused reference to word %s\n", word);
     return false;
 }
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // open input file
+    FILE *inptr = fopen(dictionary, "r");
+
+    // check if something went wrong
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "29. Could not open %s.\n", dictionary);
+        return 2;
+    }
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
