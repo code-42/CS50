7b4abb34b75a5d4fe381452d469d4f6abb7cbf06
Edward Dupre <edwd42@gmail.com>
2018-07-19 13:59:01 +0000

    add notes from walkthrough videos

--- pset6/similarities/helpers.py
+++ pset6/similarities/helpers.py
@@ -1,3 +1,10 @@
+""" splitting a string into lines
+    https://docs.python.org/3/library/stdtypes.html#string-methods
+
+    data structures to consider: list, set, ?
+    https://docs.python.org/3/tutorial/datastructures.html
+"""
+
 def lines(a, b):
     """Return lines in both a and b"""
 
@@ -6,14 +13,26 @@ def lines(a, b):
 
 
 def sentences(a, b):
-    """Return sentences in both a and b"""
+    """Return sentences in both a and b
+        split each string into sentences
+        make a list of sentences appearing in both a and b
+        'Hello there! How are you?' => ['Hello there', 'How are you?']
+        use nltk sent_tokenize
+        from nltk.tokenize import sent_tokenize
+    """
 
     # TODO
     return []
 
 
 def substrings(a, b, n):
-    """Return substrings of length n in both a and b"""
+    """Return substrings of length n in both a and b
+        n is substring length to compare
+        split each string into all substrings of length n
+        s[i:j] returns s[i] to s[j-1] does not include index j
+        write a helper function for s[i:j]
+        make a list of all substrings appearing in both a and b
+    """
 
     # TODO
     return []
