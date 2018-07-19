7ac6296c0200a8e73860f5b47958685787cb9f52
Edward Dupre <edwd42@gmail.com>
2018-07-19 13:25:12 +0000

    add pset6/similarities

--- pset6/mario/less/mario.py
+++ pset6/mario/less/mario.py
@@ -1,21 +1,19 @@
 from cs50 import get_int
 
+
 def printHash(nTimes):
     print("#" * nTimes, end='')
+
 
 def printSpace(nTimes):
     print(" " * nTimes, end='')
 
-height=-1
+
+height = -1
 while height < 0 or height > 23:
     height = get_int("Height: ")
 
-for i in range(2, height+2):
-    printSpace(height-i+1)
+for i in range(2, height + 2):
+    printSpace(height - i + 1)
     printHash(i)
     print()
-
-# print()
-
-
-
--- pset6/mario/more/mario.py
+++ pset6/mario/more/mario.py
@@ -1,24 +1,26 @@
 from cs50 import get_int
 
+
+# this function prints hash marks
 def printHash(nTimes):
     print("#" * nTimes, end='')
 
+
+# this function prints spaces
 def printSpace(nTimes):
     print(" " * nTimes, end='')
 
-height=-1
+
+# initialize height
+height = -1
 while height < 0 or height > 23:
     height = get_int("Height: ")
 
-for i in range(1, height+1):
-    printSpace(height-i)
+# loop to height + 1 and call print functions
+for i in range(1, height + 1):
+    printSpace(height - i)
     printHash(i)
     # print 2 half pyramids with 2 spaces between
     print("  ", end='')
     printHash(i)
     print()
-
-
-
-
-
