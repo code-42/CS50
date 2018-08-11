91655dfb0a3472e86b38052aba83edee7ab27706
Edward Dupre <edwd42@gmail.com>
2018-08-11 02:38:27 +0000

    add return render_template("register.html") to application.py and displays register form

--- pset7/finance/application.py
+++ pset7/finance/application.py
@@ -115,7 +115,8 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    # return apology("TODO")
+    return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
