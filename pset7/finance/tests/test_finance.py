d164a38bbd00a943eb9507b1a0b1a77f599231e3
Edward Dupre <edwd42@gmail.com>
2018-09-17 15:59:51 +0000

    add test_quote() and comment out # def test_login(self): so login happens in setUp() and successive tests can execute

--- pset7/finance/tests/test_finance.py
+++ pset7/finance/tests/test_finance.py
@@ -10,8 +10,6 @@ from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support.ui import expected_conditions as EC
 
 class TestFinance(unittest.TestCase):
 
@@ -23,11 +21,11 @@ class TestFinance(unittest.TestCase):
         opts = FirefoxOptions()
         opts.add_argument("--headless")
         self.driver = webdriver.Firefox(options=opts)
+        self.driver.implicitly_wait(30) # seconds
 
         # access the app URL
         url = "https://edwd42-cs50-code42.c9users.io:8080"
         self.driver.get(url)
-        self.driver.implicitly_wait(4) # seconds
         # html = self.driver.page_source
         # print(html)
 
@@ -37,43 +35,53 @@ class TestFinance(unittest.TestCase):
         submit_button.click()
         # time.sleep(2)
         self.driver.implicitly_wait(2) # seconds
-        print("27. sleeping for 2...")
-        print("28. self.driver.title == " + self.driver.title)
+        print("38. sleeping for 2...")
+        print("39. self.driver.title == " + self.driver.title)
 
-    def test_login(self):
+    # def test_login(self):
         # login to the app - this is in setUp() not separate test_login()
         # so makes this an integration test not unit test.  hmmm.
 
         try:
-            # print("40. self.driver.title == " + self.driver.title)
+            # print("46. self.driver.title == " + self.driver.title)
             username = self.driver.find_element_by_id("username")
-            time.sleep(1)
             username.send_keys("ee")
             password = self.driver.find_element_by_id("password")
             password.send_keys("dd")
             submit_button = self.driver.find_element_by_id("submit")
             submit_button.click()
-            time.sleep(10)
-            print("49. self.driver.title == " + self.driver.title)
+            print("53. self.driver.title == " + self.driver.title)
             assert "C$50" in self.driver.title
         except:
             print("52. cannot find Log In")
             self.driver.close()
             self.driver.quit()
 
+    def test_quote(self):
+        # navigate to the quote link
+        url = "/quote"
+        # quote_link = self.driver.find_element_by_xpath('//a[@href="/quote"]')
+        quote_link = self.driver.find_element_by_link_text("Quote")
+        quote_link.click()
+        # make sure we're on the Quote page - self.driver.title == C$50 Finance: Quote
+        print("67. self.driver.title == " + self.driver.title)
+        try:
+            symbol = self.driver.find_element_by_name("symbol")
+            symbol.send_keys("ee")
+            # <button class="btn btn-primary" type="submit" id="submit>Get Quote</button>
+            submit_button = self.driver.find_element_by_id("submit")
+            submit_button.click()
 
-
-    # def wait_for_element():
-    #     # source https://selenium-python.readthedocs.io/waits.html#explicit-waits
-    #     try:
-    #         element = WebDriverWait(self.driver, 10).until(
-    #             EC.presence_of_element_located((By.ID, "button"))
-    #         )
-    #     finally:
-    #         self.driver.close()
-    #         self.driver.quit()
+            # now we should be on the Quoted page
+            # print("76. self.driver.title == " + self.driver.title)
+            assert "Quoted" in self.driver.title
+        except:
+            print("79. cannot find Quote page")
+            self.driver.close()
+            self.driver.quit()
 
     def tearDown(self):
+        print("102. inside tearDown()")
         self.driver.close()
         self.driver.quit()
 
